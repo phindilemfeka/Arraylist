e5632d2a9811030d0fe7273dfaf029d7b014c35a
phindilemfeka <phindileportia20@gmail.com>
2020-05-07 04:46:11 +0200

    1st

---
+++ src/main/java/list.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections; //for it's min and max methods
+
+class list {
+    static void printGreatestValue() {
+
+        ArrayList <Integer>list = new ArrayList<>(3);
+        list.add(5);
+        list.add(9);
+        list.add(1);
+
+        int max;
+        int min;
+        System.out.println(list); //print as a list
+
+        for (Integer a : list) {
+            System.out.println(a); //print as individuals
+        }
+        max=Collections.max(list);
+        min=Collections.min(list);
+        System.out.println("max: " + max+ " min: "+min); //print min and max from the list
+
+
+        }
+
+    }
+
+
